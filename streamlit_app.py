0d6d0ca971c03c8e3b2cb7cc3850bd495d0a214a
Navya M <77515207+navyasrim@users.noreply.github.com>
2023-04-25 15:31:56 -0500

    Update streamlit_app.py
    added fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 #new section to display fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 
 # normalise the json response
