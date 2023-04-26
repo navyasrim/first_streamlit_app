3bede4b5d43b40c18714ce1764000a13da77420f
Navya M <77515207+navyasrim@users.noreply.github.com>
2023-04-26 13:38:51 -0500

    Update streamlit_app.py
    modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
       streamlit.error("please select a fruit to get info")
    else:
@@ -35,7 +35,7 @@ try:
    
 except URLError as e:
    streamlit.error()
-streamlit.write('The user entered ', fruit_choice)
+
 
 #new section to display fruityvice api response
 
